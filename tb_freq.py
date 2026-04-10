b614f0f6b7945345329b0b5a679be6c6ae430fa7
Diogobayer <diogobayer39@gmail.com>
2026-04-09 22:25:16 -0300

    Aula 04 Tabela frequencia completa

--- tb_freq.py
+++ tb_freq.py
@@ -17,3 +17,17 @@ freq_produto["Freq. Rel."]= freq_produto["idTransacao"] / freq_produto["idTransa
 freq_produto["Freq. Rel. Acum."] = freq_produto["Freq. Rel."].cumsum()
 
 freq_produto
+
+#%%
+df
+#%%
+
+freq_cat = df.groupby(["descCategoriaProduto"])[["idTransacao"]].count().rename(columns={"idTransacao":"Freq. Abs."})
+
+freq_cat["Freq. Abs. Acum."] = freq_cat["Freq. Abs."].cumsum()
+
+freq_cat["Freq. Relat"] = freq_cat["Freq. Abs."] / freq_cat["Freq. Abs."].sum()
+
+freq_cat["Freq. Relat. Acum."] = freq_cat["Freq. Relat"].cumsum()
+
+freq_cat
