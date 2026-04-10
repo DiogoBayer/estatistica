2357ba83778491b1dbcd9c2eef3a6c40e14b1c77
Diogobayer <diogobayer39@gmail.com>
2026-04-09 22:11:24 -0300

    Aula 04 Tabela frequencia completa

--- tb_freq.py
+++ tb_freq.py
@@ -7,4 +7,13 @@ df
 
 #%%
 
-df.groupby(["descProduto"])[["idTransacao"]].count()
+freq_produto = (df.groupby(["descProduto"])[["idTransacao"]]
+                .count())
+
+freq_produto["Freq. Abs. Acum."] = freq_produto["idTransacao"].cumsum()
+
+freq_produto["Freq. Rel."]= freq_produto["idTransacao"] / freq_produto["idTransacao"].sum()
+
+freq_produto["Freq. Rel. Acum."] = freq_produto["Freq. Rel."].cumsum()
+
+freq_produto
