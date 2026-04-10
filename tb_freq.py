86d2f9298edaffeb9ffa09e4bc0f78dd5e40ad1f
Diogobayer <diogobayer39@gmail.com>
2026-04-09 22:47:30 -0300

    Tabela no SQL

--- tb_freq.py
+++ tb_freq.py
@@ -1,9 +1,15 @@
 #%%
 
 import pandas as pd
+import sqlalchemy
+
 
 df = pd.read_csv("data/points_tmw.csv")
-df
+df.head()
+
+engine = sqlalchemy.create_engine("sqlite:///data/tmw.db")
+
+df.to_sql("points", engine, if_exists="replace", index=False)
 
 #%%
 
